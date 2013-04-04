65f96588e859d0f48a0ea9b3af9dd3869aaf0d4f
vitaly <vitgvr@gmail.com>
2013-04-04 11:55:42 +0600

    first commit

---
+++ protected/migrations/m130404_053114_create_page_table.php
@@ -0,0 +1,29 @@
+<?php
+
+class m130404_053114_create_page_table extends CDbMigration
+{
+	public function up()
+	{
+		$this->createTable('page', array(
+            'id' => 'pk',
+            'title' => 'string NOT NULL',
+            'content' => 'text',
+        ));
+	}
+
+	public function down()
+	{
+		$this->dropTable('page');
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}

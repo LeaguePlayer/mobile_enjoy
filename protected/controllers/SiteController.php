d4804e3c5439a5a481eea082b7392e2aba0c2b33
Sarvin <scorgi86@gmail.com>
2015-07-03 21:51:33 +0300

    qwe

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -105,25 +105,26 @@ class SiteController extends FrontController
 	 */
 	public function actionLogin()
 	{
-		$model = new LoginForm;
+		$this->redirect('/user/login');
+		// $model = new LoginForm;
 
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
+		// // if it is ajax validation request
+		// if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+		// {
+		// 	echo CActiveForm::validate($model);
+		// 	Yii::app()->end();
+		// }
 
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect('/block/index');
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
+		// // collect user input data
+		// if(isset($_POST['LoginForm']))
+		// {
+		// 	$model->attributes=$_POST['LoginForm'];
+		// 	// validate user input and redirect to the previous page if valid
+		// 	if($model->validate() && $model->login())
+		// 		$this->redirect('/block/index');
+		// }
+		// // display the login form
+		// $this->render('login',array('model'=>$model));
 	}
 
 	/**
@@ -131,7 +132,8 @@ class SiteController extends FrontController
 	 */
 	public function actionLogout()
 	{
-		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
+		$this->redirect('/user/logout');
+		// Yii::app()->user->logout();
+		// $this->redirect(Yii::app()->homeUrl);
 	}
 }
